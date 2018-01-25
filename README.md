a15409ba2f0a53b8eb065829dcc5cd76af2f5315
Brandon Romano <BrandonRRomano@gmail.com>
2018-01-25 12:33:09 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# CLI Target
+
+CLI Target is a command line target for [fsm](https://github.com/fsm/fsm).
+
+# License
+
+[MIT](LICENSE.md)
---
+++ cli.go
@@ -0,0 +1,41 @@
+package cli
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/fsm/fsm"
+)
+
+func Start(stateMachine fsm.StateMachine, startState string) {
+	// Create Emitter
+	emitter := &CommandLineEmitter{}
+
+	// Create Traverser
+	traverser := &CachedTraverser{
+		Data: make(map[string]interface{}, 0),
+	}
+	traverser.SetCurrentState(startState)
+	traverser.SetUUID("CLI-USER")
+
+	// Get Start State
+	currentState := stateMachine[startState](emitter, traverser)
+	currentState.EntryAction()
+
+	// Prep Reader
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		// Read Input from User
+		fmt.Print("-> ")
+		text, _ := reader.ReadString('\n')
+		text = text[:len(text)-1]
+
+		// Pass Input to State
+		currentState = stateMachine[traverser.CurrentState()](emitter, traverser)
+		newState := currentState.Transition(text)
+		newState.EntryAction()
+		currentState = newState
+		traverser.SetCurrentState(currentState.Slug)
+	}
+}
