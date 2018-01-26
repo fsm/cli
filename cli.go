45ebf9ca674918f4cee2074611c620fa0bc30782
Brandon Romano <BrandonRRomano@gmail.com>
2018-01-26 11:57:34 -0800

    Delete embedded traverser, allow user to pass in

--- cli.go
+++ cli.go
@@ -2,30 +2,37 @@ package cli
 
 import (
 	"bufio"
+	"crypto/rand"
 	"fmt"
 	"os"
 
 	"github.com/fsm/fsm"
 )
 
-func Start(stateMachine fsm.StateMachine, startState string) {
+// Start begins the CLI target for a fsm.StateMachine
+func Start(stateMachine fsm.StateMachine, store fsm.Store) {
 	// Create Emitter
-	emitter := &CommandLineEmitter{}
+	emitter := &emitter{}
 
-	// Create Traverser
-	traverser := &CachedTraverser{
-		Data: make(map[string]interface{}, 0),
+	// Get Traverser
+	uuid := uuid()
+	newTraverser := false
+	traverser, err := store.FetchTraverser(uuid)
+	if err != nil {
+		traverser, _ = store.CreateTraverser(uuid)
+		traverser.SetCurrentState("start")
+		newTraverser = true
 	}
-	traverser.SetCurrentState(startState)
-	traverser.SetUUID("CLI-USER")
 
 	// Get Start State
-	currentState := stateMachine[startState](emitter, traverser)
-	currentState.EntryAction()
+	currentState := stateMachine[traverser.CurrentState()](emitter, traverser)
+	if newTraverser {
+		currentState.EntryAction()
+	}
 
 	// Prep Reader
 	reader := bufio.NewReader(os.Stdin)
-	for {
+	for currentState != nil {
 		// Read Input from User
 		fmt.Print("-> ")
 		text, _ := reader.ReadString('\n')
@@ -38,4 +45,16 @@ func Start(stateMachine fsm.StateMachine, startState string) {
 		currentState = newState
 		traverser.SetCurrentState(currentState.Slug)
 	}
+}
+
+// uuid generates a UUID that isn't particularly compliant
+// to any spec but is more than good enough for our use case
+func uuid() string {
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		return ""
+	}
+	str := fmt.Sprintf("%X-%X-%X-%X-%X", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+	return str
 }
--- emitter.go
+++ emitter.go
@@ -4,33 +4,44 @@ import (
 	"errors"
 	"fmt"
 	"reflect"
+	"time"
 
 	"github.com/fsm/emitable"
 )
 
-type CommandLineEmitter struct{}
+// We add some sleep time between each type of message to make it feel more natural
+// Not doing anything fancy here, and keeping it somewhat quick as this target will
+// be used mostly in development.
+const sleepTime = 600
 
-func (c *CommandLineEmitter) Emit(i interface{}) error {
+type emitter struct{}
+
+func (e *emitter) Emit(i interface{}) error {
 	switch v := i.(type) {
 
 	case string:
 		fmt.Println(v)
+		e.Emit(emitable.Sleep{LengthMillis: sleepTime})
 		return nil
 
 	case emitable.Audio:
 		fmt.Println("Audio:", v.URL)
+		e.Emit(emitable.Sleep{LengthMillis: sleepTime})
 		return nil
 
 	case emitable.File:
 		fmt.Println("File:", v.URL)
+		e.Emit(emitable.Sleep{LengthMillis: sleepTime})
 		return nil
 
 	case emitable.Image:
 		fmt.Println("Image:", v.URL)
+		e.Emit(emitable.Sleep{LengthMillis: sleepTime})
 		return nil
 
 	case emitable.Video:
 		fmt.Println("Video:", v.URL)
+		e.Emit(emitable.Sleep{LengthMillis: sleepTime})
 		return nil
 
 	case emitable.QuickReply:
@@ -44,10 +55,15 @@ func (c *CommandLineEmitter) Emit(i interface{}) error {
 		fmt.Println(" ]")
 		return nil
 
+	case emitable.Sleep:
+		time.Sleep(time.Duration(v.LengthMillis) * time.Millisecond)
+		return nil
+
 	case emitable.Typing:
 		if v.Enabled {
 			fmt.Println("...")
 		}
+		e.Emit(emitable.Sleep{LengthMillis: sleepTime})
 		return nil
 	}
 	return errors.New("CommandLineEmitter cannot handle " + reflect.TypeOf(i).String())
